0539ec1e92040061cd21108b87db1c20fc6c9eb1
Bishal Rana <rbishal50@gmail.com>
2021-03-25 17:38:42 +0545

    Add default export for base component

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,4 +1,4 @@
-import { Component } from "./base-component.js";
+import Component from "./base-component.js";
 import { Project } from "../models/project.js";
 import { Draggable } from "../models/drag-drop.js";
 import { AutoBind } from "../decorators/autobind.js";
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,4 +1,4 @@
-import { Component } from "./base-component.js";
+import Component from "./base-component.js";
 import { projectState } from "../state/project-state.js";
 import { Project, ProjectStatus } from "../models/project.js";
 import { ProjectItem } from "./project-item.js";
